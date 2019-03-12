c0191b451c7bc34f1235964292885c731f257fbe
Ryan Smith <ryan.l.smith4@gmail.com>
2019-03-12 14:48:06 -0700

    Fixed bad db connection

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    SQUALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQUALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
